1ccfc6ffa271ea3e41ea101f06393eed04fe2a07
CezSew <csewerynski@interia.pl>
2019-07-14 22:26:57 +0200

    Added styled components

--- resources/assets/js/layouts/FormPage.jsx
+++ resources/assets/js/layouts/FormPage.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 
 import { Card, CardContent } from 'components'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  margin-top: 100px;
+`
 
 export const FormPageLayout = props => (
-  <Card className="max-w-md mt-20 mx-auto">
-    <CardContent>
-      <h1 className="text-center text-grey-darkest mb-4">{props.title}</h1>
-      {props.children}
-    </CardContent>
-  </Card>
+  <Wrapper>
+    <Container component="main" maxWidth="xs" className="jss1">
+      <Card className="max-w-md mt-20 mx-auto">
+        <CardContent>
+          <Typography variant="h3" component="h1">
+            {props.title}
+          </Typography>
+          {props.children}
+        </CardContent>
+      </Card>
+    </Container>
+  </Wrapper>
 )
