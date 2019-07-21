95369811df4d2b17af811da00d970c15c3abafee
CezSew <csewerynski@interia.pl>
2019-07-21 21:57:40 +0200

    Moved main logical container to app.jsx

--- resources/assets/js/app.jsx
+++ resources/assets/js/app.jsx
@@ -10,6 +10,9 @@ import Question from './components/Question/Question';
 import Choose from './components/Choose/Choose';
 import Load from './components/Load/Load';
 
+import * as utils from 'utils/index';
+import { Link } from 'react-router-dom';
+
 import {
     AuthGuard,
     FlashMessageRoot,
@@ -44,79 +47,197 @@ const Loading = () => (
 const OverviewWithDashboard = withDashboard(Overview);
 const SettingsWithDashboard = withDashboard(SettingsRoutes);
 
-export const App = props => (
-    <Provider store={store}>
-        <Suspense fallback={<Loading />}>
-            <FlashMessageRoot />
-            <ConnectedRouter history={browserHistory}>
-                <ModalProviderWrapper>
-                    <ModalRoot />
-                    <Switch>
-                        <Route
-                            exact
-                            path="/login"
-                            render={() => (
-                                <FormPageLayout>
-                                    <LogIn />
-                                </FormPageLayout>
-                            )}
-                        />
-                        <Route
-                            exact
-                            path="/signup"
-                            render={() => (
-                                <FormPageLayout title="Sign Up">
-                                    <SignUp />
-                                </FormPageLayout>
-                            )}
-                        />
-                        <Route
-                            exact
-                            path="/forgot-password"
-                            render={() => (
-                                <FormPageLayout title="Forgot Password">
-                                    <ForgotPassword />
-                                </FormPageLayout>
-                            )}
-                        />
-                        <Route
-                            exact
-                            path="/reset-password/:resetToken"
-                            render={() => (
-                                <FormPageLayout title="Reset Password">
-                                    <PasswordReset />
-                                </FormPageLayout>
-                            )}
-                        />
+export class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-                        <Route
-                            exact
-                            path="/question"
-                            render={() => (
-                                <Question />
-                            )}
-                        />
+        // const ModalExample = props => <div>{props.message}</div>
+        const options = utils.getOptions();
+        this.generateNewTest = this.generateNewTest.bind(this);
 
-                        <Route
-                            exact
-                            path="/choose-test"
-                            render={() => (
-                                <Choose />
-                            )}
-                        />
+        this.state = {
+            step: 'start',
+            questionIndex: 0,
+            question: {
+                data: {},
+                correctIndex: '',
+                currentQuestionNumber: '',
+                isTheAnswerCorrect: false
+            },
+            stats: [],
+            randomModeStats: [],
+            questionsCount: '',
+            questionTimeout: 1500,
+            options: options
+        };
+    }
 
-                        <Route path="/load-questions" exact render={ () =>
-                            <Load />
-                        }/>
+    /**
+      * Sets state values dependant on question
+      */
+    prepareQuestion () {
+        let question = {...utils.getQuestions(this.state.question.data, this.state.chosenTest)};
+        let currentQuestionNumber = this.state.question.currentQuestionNumber;
+        let questionsCount = utils.getQuestionsCount(question);
+        let questionNum = utils.setQuestionNumber(currentQuestionNumber, questionsCount);
+        let step = utils.setStep(questionNum, currentQuestionNumber, questionsCount, this.state.stats);
+        let repeat = false;
 
-                        {/* Dashboard routes */}
-                        <Route exact path="/" component={OverviewWithDashboard} />
-                        <Route path="/settings" component={SettingsWithDashboard} />
-                        {/* 404 route */}
-                        <Route path="*" exact={true} render={() => <NotFound />} />
-                    </Switch>
-                </ModalProviderWrapper>
-            </ConnectedRouter>
-        </Suspense>
-    </Provider>
-);
+        if (this.state.repeat || (utils.areSomeAnswersIncorrect(this.state.stats) && utils.isLastQuestion(currentQuestionNumber, questionsCount))) {
+            step = 'question';
+            repeat = true;
+            questionNum = utils.getRandomQuestionNumber(this.state.question.data, questionsCount, this.state.stats, currentQuestionNumber);
+        }
+        this.setState({
+            step: step,
+            question: { data: question, currentQuestionNumber: questionNum, correctIndex: question[questionNum].correct },
+            questionsCount: questionsCount,
+            chosenTest: 'test_test',
+            repeat: repeat
+        }, function() {
+            step === 'finish'
+                ? this.props.history.push('/summary')
+                : this.props.history.push('/question');
+        });
+    }
+
+    /**
+     * Sets new state of application step
+     * @param {string}
+     */
+    changeAppStep(step) {
+        this.setState({step: step});
+    }
+
+    /**
+     * Sets new state values regarding question
+     * @param {string} testName
+     */
+    generateNewTest(testName) {
+        let question = utils.loadQuestion(testName);
+        let questionsCount = Object.keys(question).length;
+
+        this.setState({
+            step: 'question',
+            question: { data: question, currentQuestionNumber: 1, correctIndex: question[1].correct },
+            questionsCount: questionsCount,
+            chosenTest: testName,
+            stats: [],
+            repeat: false
+        }, function() {
+            this.props.history.push('/question');
+        });
+    }
+
+    /**
+     * Sets a new state. After new state has been set, trigger new question render, after timeout.
+     * @param {boolean} isCorrect
+     */
+    handleAnswer(isCorrect) {
+        let currentNumber = this.state.question.currentQuestionNumber;
+        let correctAnswersCount = utils.getCorrectAnswersCountOfQuestion(currentNumber - 1, this.state.stats);
+        this.setState({
+            stats: [...this.state.stats, [currentNumber, isCorrect, utils.setCorrectAnswersCountOfQuestion(correctAnswersCount, isCorrect)]]
+        }, () => {
+            setTimeout(() => {
+                this.prepareQuestion();
+            }, this.state.questionTimeout);
+        });
+    }
+
+    /**
+     * Handle click of the main button, displaying on the first view
+     */
+    handleButtonClick () {
+        this.prepareQuestion();
+    }
+    render() {
+        return (
+            <Provider store={store}>
+                <Suspense fallback={<Loading />}>
+                    <FlashMessageRoot />
+                    <ConnectedRouter history={browserHistory}>
+                        <ModalProviderWrapper>
+                            <ModalRoot />
+                            <Switch>
+                                <Route
+                                    exact
+                                    path="/login"
+                                    render={() => (
+                                        <FormPageLayout>
+                                            <LogIn />
+                                        </FormPageLayout>
+                                    )}
+                                />
+                                <Route
+                                    exact
+                                    path="/signup"
+                                    render={() => (
+                                        <FormPageLayout title="Sign Up">
+                                            <SignUp />
+                                        </FormPageLayout>
+                                    )}
+                                />
+                                <Route
+                                    exact
+                                    path="/forgot-password"
+                                    render={() => (
+                                        <FormPageLayout title="Forgot Password">
+                                            <ForgotPassword />
+                                        </FormPageLayout>
+                                    )}
+                                />
+                                <Route
+                                    exact
+                                    path="/reset-password/:resetToken"
+                                    render={() => (
+                                        <FormPageLayout title="Reset Password">
+                                            <PasswordReset />
+                                        </FormPageLayout>
+                                    )}
+                                />
+
+                                <Route
+                                    exact
+                                    path="/question"
+                                    render={() => (
+                                        <Question />
+                                    )}
+                                />
+
+                                <Route
+                                    exact
+                                    path="/choose-test"
+                                    render={() => (
+                                        <Choose />
+                                    )}
+                                />
+
+                                <Route path="/load-questions" exact render={ () =>
+                                    <Load />
+                                }/>
+
+                                {/* Dashboard routes */}
+                                <Route exact path="/"
+                                    render={() => (
+                                        <OverviewWithDashboard
+                                            state={this.state}
+                                            handleAnswer={this.handleAnswer}
+                                            generateNewTest={this.generateNewTest}
+                                            changeAppStep={this.changeAppStep}
+                                            prepareQuestion={this.prepareQuestion}
+                                            handleButtonClick={this.handleButtonClick}
+                                            utils={utils}
+                                        />
+                                    )}/>
+                                <Route path="/settings" component={SettingsWithDashboard} />
+                                {/* 404 route */}
+                                <Route path="*" exact={true} render={() => <NotFound />} />
+                            </Switch>
+                        </ModalProviderWrapper>
+                    </ConnectedRouter>
+                </Suspense>
+            </Provider>
+        );
+    }
+}
